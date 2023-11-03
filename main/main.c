bccc1d196f60d95598c755037999c3760fcfd35c
tjadczak <tomasz.jadczak1996@gmail.com>
2023-11-03 19:42:38 +0100

    Modified project name in makefiles, added empty headers and c files for future libraries

--- main/main.c
+++ main/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "I2C.h"
+#include "Interrupts.h"
+#include "LcdDisplay.h"
 
 
-void app_main(void)
-{
-    int i = 0;
+void app_main(void){
+    // Initialize variables
+    // Initialize input and output ports
+    // Initialize I2C bus
+    // Initialize BMP280 sensor
+    // Initialize LCD display
+    // Initialize interrupts to read and display measurements on demand (button)
     while (1) {
-        printf("[%d] Hello world!\n", i);
-        i++;
+        printf("Hello world!\n");
         vTaskDelay(5000 / portTICK_PERIOD_MS);
+	//read values from BMP280
+	//display measurements on LCD display
+	//go to deep sleep for X seconds
     }
 }
