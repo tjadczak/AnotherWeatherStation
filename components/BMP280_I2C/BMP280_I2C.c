0d4ad0d95cef90a55dacc37de325beea3bca19f6
tjadczak <tomasz.jadczak1996@gmail.com>
2023-11-07 13:16:48 +0100

    fixing compensation func issues

--- components/BMP280_I2C/BMP280_I2C.c
+++ components/BMP280_I2C/BMP280_I2C.c
@@ -15,11 +15,13 @@
 #define BMP280_CONFIG_REG_ADDR      0xF5        	        
 
 // Returns temperature in DegC, resolution is 0.01 DegC. Output value of “5123” equals 51.23 DegC.
+// t_fine carries fine temperature as global value
+BMP280_S32_t t_fine;
 BMP280_S32_t bmp280_compensate_T_int32(BMP280_S32_t adc_T){
-    BMP280_S32_t var1, var2, T, t_fine;
-    var1 = ((((adc_T>>3) – ((BMP280_S32_t)dig_T1<<1))) * ((BMP280_S32_t)dig_T2)) >> 11;
-    var2 = (((((adc_T>>4) – ((BMP280_S32_t)dig_T1)) * ((adc_T>>4) – ((BMP280_S32_t)dig_T1))) >> 12) *
-    ((BMP280_S32_t)dig_T3)) >> 14;
+    BMP280_S32_t var1, var2, T;
+    var1 = ((((adc_T>>3) – ((BMP280_S32_t)comp.dig_T1<<1))) * ((BMP280_S32_t)comp.dig_T2)) >> 11;
+    var2 = (((((adc_T>>4) – ((BMP280_S32_t)comp.dig_T1)) * ((adc_T>>4) – ((BMP280_S32_t)comp.dig_T1))) >> 12) *
+    ((BMP280_S32_t)comp.dig_T3)) >> 14;
     t_fine = var1 + var2;
     T = (t_fine * 5 + 128) >> 8;
     return T;
@@ -30,19 +32,19 @@ BMP280_S32_t bmp280_compensate_T_int32(BMP280_S32_t adc_T){
 BMP280_U32_t bmp280_compensate_P_int64(BMP280_S32_t adc_P){
     BMP280_S64_t var1, var2, p;
     var1 = ((BMP280_S64_t)t_fine) – 128000;
-    var2 = var1 * var1 * (BMP280_S64_t)dig_P6;
-    var2 = var2 + ((var1*(BMP280_S64_t)dig_P5)<<17);
-    var2 = var2 + (((BMP280_S64_t)dig_P4)<<35);
-    var1 = ((var1 * var1 * (BMP280_S64_t)dig_P3)>>8) + ((var1 * (BMP280_S64_t)dig_P2)<<12);
-    var1 = (((((BMP280_S64_t)1)<<47)+var1))*((BMP280_S64_t)dig_P1)>>33;
+    var2 = var1 * var1 * (BMP280_S64_t)comp.dig_P6;
+    var2 = var2 + ((var1*(BMP280_S64_t)comp.dig_P5)<<17);
+    var2 = var2 + (((BMP280_S64_t)comp.dig_P4)<<35);
+    var1 = ((var1 * var1 * (BMP280_S64_t)comp.dig_P3)>>8) + ((var1 * (BMP280_S64_t)comp.dig_P2)<<12);
+    var1 = (((((BMP280_S64_t)1)<<47)+var1))*((BMP280_S64_t)comp.dig_P1)>>33;
     if (var1 == 0){
         return 0; // avoid exception caused by division by zero
     }
     p = 1048576-adc_P;
     p = (((p<<31)-var2)*3125)/var1;
-    var1 = (((BMP280_S64_t)dig_P9) * (p>>13) * (p>>13)) >> 25;
-    var2 = (((BMP280_S64_t)dig_P8) * p) >> 19;
-    p = ((p + var1 + var2) >> 8) + (((BMP280_S64_t)dig_P7)<<4);
+    var1 = (((BMP280_S64_t)comp.dig_P9) * (p>>13) * (p>>13)) >> 25;
+    var2 = (((BMP280_S64_t)comp.dig_P8) * p) >> 19;
+    p = ((p + var1 + var2) >> 8) + (((BMP280_S64_t)comp.dig_P7)<<4);
     return (BMP280_U32_t)p;
 }
 
@@ -62,18 +64,18 @@ esp_err_t BMP280_init(void){
     return i2c_driver_install(I2C_MASTER_PORT, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);    
 }
 
-uint8_t BMP280_check_ID(void){
-    uint8_t ID;
+char BMP280_check_ID(void){
+    char ID;
     BMP280_register_read(BMP280_WHO_AM_I_REG_ADDR, &ID, 1);
     return ID;
 }
 
 void BMP280_read_compensation_data(void){
-    BMP280_compensation comp;
+    BMP280_compensation comp2;
     char data[2];
-    BMP280_register_read(0x88, &data, len(data));
-    comp.dig_T1 == (uint16_t)data[1]<<8+data[0];
-    printf("comp.dig_T1 == %d", comp.dig_T1);
+    BMP280_register_read(0x88, &data[0], 2);
+    comp2.dig_T1 == (uint16_t)data[1]<<8+data[0];
+    printf("comp.dig_T1 == %d", comp2.dig_T1);
 }
 
 esp_err_t BMP280_set_ctrl_meas(void){
@@ -91,7 +93,7 @@ esp_err_t BMP280_set_config(void){
 }
 */
 
-void BMP280_register_read(uint8_t reg_addr, uint8_t *data, size_t len)
+void BMP280_register_read(uint8_t reg_addr, char *data, size_t len)
 {
     ESP_ERROR_CHECK(i2c_master_write_read_device(I2C_MASTER_NUM, BMP280_SENSOR_ADDR, &reg_addr, 1, data, len, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS));
 }
--- components/BMP280_I2C/include/BMP280_I2C.h
+++ components/BMP280_I2C/include/BMP280_I2C.h
@@ -24,8 +24,13 @@ typedef struct BMP280_compensation {
     int16_t dig_P9;
 } BMP280_compensation;
 
+const BMP280_compensation comp = {.dig_T1 = 0, .dig_T2 = 0, .dig_T3 = 0,
+                                  .dig_P1 = 0, .dig_P2 = 0, .dig_P3 = 0, 
+                                  .dig_P4 = 0, .dig_P5 = 0, .dig_P6 = 0, 
+                                  .dig_P7 = 0, .dig_P8 = 0, .dig_P9 = 0};
+
 esp_err_t BMP280_init(void);
-uint8_t BMP280_check_ID(void);
+char BMP280_check_ID(void);
 esp_err_t BMP280_set_ctrl_meas(void);
 esp_err_t BMP280_set_config(void);
 esp_err_t BMP280_deinit(void);
@@ -33,7 +38,7 @@ void BMP280_read_compensation_data(void);
 //void BMP280_read_temp_and_press(*float temp, *uint8_t press);
 
 
-void BMP280_register_read(uint8_t reg_addr, uint8_t *data, size_t len);
+void BMP280_register_read(uint8_t reg_addr, char *data, size_t len);
 esp_err_t BMP280_register_write_byte(uint8_t reg_addr, uint8_t data);
 
 
